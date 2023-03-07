daf26bddf17bee1983ac70ae4c5e7f51f3fd2bc8
test <test@neto.com>
2023-03-07 10:49:08 +0300

    Третий комит - добавил обновление контакта_07.03

--- update_name_phone.py
+++ update_name_phone.py
@@ -11,17 +11,40 @@ header = contacts_list[0]
 data = contacts_list[1:]
 
 
-def text_main():
-    print(f'header:{header}')
-    print('data:')
-    for contact in data:
-        print(contact)
+class Contact:
+    def __init__(self, firstname, lastname, surname, organisation, position, phone, email):
+        self.firstname = firstname
+        self.lastname = lastname
+        self.surname = surname
+        self.organisation = organisation
+        self.position = position
+        self.phone = phone
+        self.email = email
+
+    def update(self, other):
+        self.firstname = self.firstname or other.firstname
+        self.lastname = self.lastname or other.lastname
+        self.surname = self.surname or other.surname
+        self.organisation = self.organisation or other.organisation
+        self.position = self.position or other.position
+        self.phone = self.phone or other.phone
+        self.email = self.email or other.email
+
+    def __eq__(self, other):
+        return self.firstname == other.firstname and self.lastname == other.lastname
+
+    def __str__(self):
+        return f'{self.firstname}, {self.lastname}, {self.surname}, {self.organisation}, {self.position}, {self.phone}, {self.email}'
+
+    def __repr__(self):
+        return str(self)
+
 
 # Обновление ФИО
-def update_name():
-    cont = []
-    contact_all = []
 
+if __name__ == '__main__':
+
+    storage = []
     for contact in data:
         firstname, lastname, surname = contact[:3]
         temp = f'{firstname} {lastname} {surname}'.strip()
@@ -39,25 +62,31 @@ def update_name():
             firstname = name_all_list[0]
             name_list = [firstname, "", ""]
 
-
         # Регулярка для телефона
         phone_bad = contact[5]
         pattern = r"(\+7|7|8)(\s\(|\s|\(|[ ]*)(\d{3})(\)\s|-|\)|[1]*)(\d{3})(-|\s|[1]*)(\d{2})(-|[1]*)(\d{2})($|\s\(*)([а-ё]+\.\s\d+|[1]*)(\)|[1]*)"
         phone_result = re.sub(pattern, r"+7(\3)\5-\7-\9 \11", phone_bad)
 
         cont = [name_list[0], name_list[1], name_list[2], contact[3], contact[4], phone_result, contact[6]]
-        contact_all.append(cont)
 
-    return contact_all
+        # ********* Обновляем контакт
+        contact = Contact(cont[0], cont[1], cont[2], cont[3], cont[4], cont[5], cont[6])
+        if contact in storage:
+            i = storage.index(contact)
+            storage[i].update(contact)
+        else:
+            storage.append(contact)
 
-result = update_name()
+    pprint(storage)
 
-## 2. Сохраните получившиеся данные в другой файл.
-with open("phonebook_result.csv", "w") as f:
-    datawriter = csv.writer(f, delimiter=',')
 
-    ## Вместо contacts_list подставьте свой список:
-    datawriter.writerows(result)
+
+
+    ## 2. Сохраните получившиеся данные в другой файл.
+    with open("phonebook_result.csv", "w") as f:
+        datawriter = csv.writer(f, delimiter=',')
+        for c in storage:
+            datawriter.writerow([c.firstname, c.lastname, c.surname, c.organisation, c.position, c.phone, c.email])
 
 
 
