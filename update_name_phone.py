7edede0d471fadc3bb9bb7540821984654723b70
test <test@neto.com>
2023-03-12 11:36:31 +0300

    Итоговый комит

--- update_name_phone.py
+++ update_name_phone.py
@@ -1,93 +1,102 @@
-from pprint import pprint
-import re
 import csv
+import re
 
 
-with open("phonebook_raw.csv") as f:
-    rows = csv.reader(f, delimiter=",")
+with open('phonebook_raw.csv') as temp:
+    rows = csv.reader(temp)
     contacts_list = list(rows)
 
 header = contacts_list[0]
 data = contacts_list[1:]
 
 
+# Создадим класс Контакта.
+# Затем, будем создавать экземпляры класса
+# Затем добавим метод сравнения двух экземпляров по Фамилии и Имени
+# И если они совпадут, то записываем в недостающие поля из первого экземпляра - данные из второго экземпляра
+# И второй экземпляр, из которого взяли дополнительные данные - не записываем в результат
+
 class Contact:
-    def __init__(self, firstname, lastname, surname, organisation, position, phone, email):
+    def __init__(self, firstname, lastname, surname, organization, position, phone, email):
         self.firstname = firstname
         self.lastname = lastname
         self.surname = surname
-        self.organisation = organisation
+        self.organization = organization
         self.position = position
         self.phone = phone
         self.email = email
 
-    def update(self, other):
-        self.firstname = self.firstname or other.firstname
-        self.lastname = self.lastname or other.lastname
-        self.surname = self.surname or other.surname
-        self.organisation = self.organisation or other.organisation
-        self.position = self.position or other.position
-        self.phone = self.phone or other.phone
-        self.email = self.email or other.email
+    def __str__(self):
+        return f'{self.firstname}, {self.lastname}, {self.surname}, {self.organization}, {self.position}, {self.phone}, {self.email}'
 
+    # Данный метод возвращает True или False, если выполняется условие сравнения двух экземпляров
     def __eq__(self, other):
         return self.firstname == other.firstname and self.lastname == other.lastname
-
-    def __str__(self):
-        return f'{self.firstname}, {self.lastname}, {self.surname}, {self.organisation}, {self.position}, {self.phone}, {self.email}'
 
     def __repr__(self):
         return str(self)
 
-
-# Обновление ФИО
-
-if __name__ == '__main__':
-
-    storage = []
-    for contact in data:
-        firstname, lastname, surname = contact[:3]
-        temp = f'{firstname} {lastname} {surname}'.strip()
-        name_all_list = temp.split()
-
-        if len(name_all_list) == 3:
-            firstname, lastname, surname = name_all_list[0:3]
-            name_list = [firstname, lastname, surname]
-
-        elif len(name_all_list) == 2:
-            firstname, lastname = name_all_list[0:2]
-            name_list = [firstname, lastname, ""]
-
-        else:
-            firstname = name_all_list[0]
-            name_list = [firstname, "", ""]
-
-        # Регулярка для телефона
-        phone_bad = contact[5]
-        pattern = r"(\+7|7|8)(\s\(|\s|\(|[ ]*)(\d{3})(\)\s|-|\)|[1]*)(\d{3})(-|\s|[1]*)(\d{2})(-|[1]*)(\d{2})($|\s\(*)([а-ё]+\.\s\d+|[1]*)(\)|[1]*)"
-        phone_result = re.sub(pattern, r"+7(\3)\5-\7-\9 \11", phone_bad)
-
-        cont = [name_list[0], name_list[1], name_list[2], contact[3], contact[4], phone_result, contact[6]]
-
-        # ********* Обновляем контакт
-        contact = Contact(cont[0], cont[1], cont[2], cont[3], cont[4], cont[5], cont[6])
-        if contact in storage:
-            i = storage.index(contact)
-            storage[i].update(contact)
-        else:
-            storage.append(contact)
-
-    pprint(storage)
+    # Данный метод берет первый экземпляр и второй и записывает на место аргумента первого экземпляра (если там пусто) данные второго экземпляра
+    def update(self, other):
+        self.firstname = self.firstname or other.firstname
+        self.lastname = self.lastname or other.lastname
+        self.surname = self.surname or other.surname
+        self.organization = self.organization or other.organization
+        self.position = self.position or other.position
+        self.phone = self.phone or other.phone
+        self.email = self.email or other.email
 
 
 
 
-    ## 2. Сохраните получившиеся данные в другой файл.
-    with open("phonebook_result.csv", "w") as f:
-        datawriter = csv.writer(f, delimiter=',')
-        for c in storage:
-            datawriter.writerow([c.firstname, c.lastname, c.surname, c.organisation, c.position, c.phone, c.email])
+# Приводим ФИО к нужному виду: 'firstname', 'lastname', 'surname'
+
+storage = []
+for contact in data:
+    first, last, sur = contact[0:3]
+    temp = f'{first} {last} {sur}'
+    name_fio = temp.split()
+
+    if len(name_fio) == 3:
+        firstname, lastname, surname = name_fio[0:3]
+        name_result = [firstname, lastname, surname]
+
+    elif len(name_fio) == 2:
+        firstname, lastname = name_fio[0:2]
+        name_result = [firstname, lastname, 'None']
+
+    else:
+        firstname = name_fio[0]
+        name_result = [firstname, 'None', 'None']
 
 
+    # Регулярка для телефонов. Привести к виду: +7(999)999-99-99 доб. 9999
+    phone_bad = contact[5]
+    pattern = r"(\+7|7|8)(\s\(|\s|\(|[ ]*)(\d{3})(\)\s|-|\)|[1]*)(\d{3})(-|\s|[1]*)(\d{2})(-|[1]*)(\d{2})($|\s\(*)([а-ё]+\.\s\d+|[1]*)(\)|[1]*)"
+    phone_result = re.sub(pattern, r"+7(\3)\5-\7-\9 \11", phone_bad)
+
+    # Объединяем контакт: lastname,firstname,surname,organization,position,phone,email
+    cont = [name_result[0], name_result[1], name_result[2], contact[3], contact[4], phone_result, contact[6]]
 
 
+    # Обновляем контакт - объединить все дублирующиеся записи, если совпали Фамилия и Имя/
+    # Один экземпляр - это один элекмент в списке Storage. Т.е. первый подный контакт (ФИО, номер, почта и т.д.) - это Storage[0], Следующий экземпляр - это Storage[1]
+    contact = Contact(cont[0], cont[1], cont[2], cont[3], cont[4], cont[5], cont[6])
+
+    # Как происходит проверка:
+    # Метод __eq__ Сравнивает 2 экземпляра между собой First и Last. И если True - то выполняется условие if (ниже)
+    # Создался экземпляр contact. Если в списке Storage - уже лежит экземпляр contact, у которого Фамилия и Имя такие же, то считается индекс, где стоит этот экземпляр (первый)
+    # И затем берется этот элемент списка (по факту это экземпляр) ставится точка и у этого экземпляра вызыается наш метод Update, к которому нужно указать еще текущий экземпляр (второй)
+
+    if contact in storage:
+       index = storage.index(contact)
+       storage[index].update(contact)
+
+    else:
+        storage.append(contact)
+
+# Сохраняем в файл итог
+with open('result.csv', 'w', newline='') as temp:
+    res = csv.writer(temp)
+    for cont_res in storage:
+        res.writerow([cont_res.firstname, cont_res.lastname, cont_res.surname, cont_res.organization, cont_res.position, cont_res.phone, cont_res.email])
